ad05c88b4001421ba12dac580d15f73426fa9daf
Paul Murray <paulmurray85@gmail.com>
2021-02-13 10:37:52 -0800

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Nav, NavLink } from "./NavbarElements";
+
+
+
+function Navbar() {
+    return (
+        <>
+        <Nav>
+            <NavLink to="/">Pizza</NavLink>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar
